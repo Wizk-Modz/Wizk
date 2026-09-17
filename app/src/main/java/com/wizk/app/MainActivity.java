af4a7b249a96b897caff74a9e2166335da6760c8
Wizk Bytes <dangchinhcbg@gmail.com>
2026-09-17 14:35:38 +0700

    Wizk Bytes

--- app/src/main/java/com/wizk/app/MainActivity.java
+++ app/src/main/java/com/wizk/app/MainActivity.java
@@ -1,20 +1,6 @@
 package com.wizk.app;
 
-import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.wizk.app.databinding.ActivityMainBinding;
 
